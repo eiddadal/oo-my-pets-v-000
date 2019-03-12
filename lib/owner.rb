eea390f99428d2882f573d5ade8cfe860515491f
Dalya Eidda <dalia.eidda@gmail.com>
2019-03-12 20:47:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,49 @@
 class Owner
-  # code goes here
+ attr_accessor :pets
+ 
+  @@all = []
+  @count = 0
+  
+  def initialize(pets)
+    @pets = {} 
+    @@all << self 
+    @count += 1
+  end 
+  
+  def say_species 
+  end 
+  
+  def name 
+  end 
+  
+  def pets 
+    pets[:species]
+  end 
+  
+  def buy_fish(name) 
+  end 
+  
+  def buy_cat(name) 
+  end 
+  
+  def buy_dog(name)
+  end 
+  
+  def walk_dogs 
+  end 
+  
+  def play_with_cats 
+  end 
+  
+  def feed_fish 
+  end 
+  
+  def sell_pets
+  end 
+  
+  def list_pets 
+  end 
+  
+  def self.reset_all 
+  end 
 end
